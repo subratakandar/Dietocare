a4f9bba2c26397826381f818869d9bb6a130bada
subratakandar <subrata.kandar5@gmail.com>
2025-06-25 14:57:16 +0530

    update index.html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $('#header').load('common-layout/header.html');
 $('#footer').load('common-layout/footer.html');
 // ==shop-cart counter section
-let count = 0;
+let count = 1;
 
 function increase() {
   count++;
   document.getElementById("count").innerText = count;
 }
 
-// function decrease() {
-//   count--;
-//   document.getElementById("count").innerText = count;
-// }
+function decrease() {
+  count--;
+  document.getElementById("count").innerText = count;
+}
